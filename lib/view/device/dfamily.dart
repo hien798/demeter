8ea6a2b567c1d1663152277561f61c411f178178
Hiên Hiên <hien798@gmail.com>
2020-01-14 00:25:07 +0700

    add dfamily

--- lib/view/device/dfamily.dart
+++ lib/view/device/dfamily.dart
@@ -19,7 +19,7 @@ class _DFamilyState extends State<DFamily> {
       appBar: AppBar(
         backgroundColor: Color(0xffeeeeee),
         title: Text(
-          'Pump',
+          'DFamily',
           style: TextStyle(color: Colors.black),
         ),
         centerTitle: true,
@@ -36,48 +36,86 @@ class _DFamilyState extends State<DFamily> {
         ],
       ),
       backgroundColor: Colors.white,
-      body:
+      body: Container(
+        padding: EdgeInsets.all(scaleWidth(context, 24)),
+        child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Container(
+                child: _buildDevice('Pump $index'),
+              );
+            }),
+      ),
     );
   }
 
   Widget _buildDevice(String name) {
-    return Column(
-      children: <Widget>[
-        SizedBox(height: scaleWidth(context, 106)),
-        Container(
-          margin: EdgeInsets.all(24),
-          width: scaleWidth(context, 248),
-          height: scaleWidth(context, 248),
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/pump.png'),
-                fit: BoxFit.cover,
+    return Container(
+      height: scaleWidth(context, 215),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Text(
+              name,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: scaleWidth(context, 16),
+                fontWeight: FontWeight.w600,
               ),
-              borderRadius: BorderRadius.circular(scaleWidth(context, 30))),
-        ),
-        Expanded(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+          ),
+          Stack(
+            alignment: Alignment.topCenter,
             children: <Widget>[
-              Expanded(child: _buildPumpDevice('History', Icons.history)),
-              Expanded(child: _buildPumpDevice('Timer', Icons.timer)),
-              Expanded(child: _buildPumpDevice('Alarm', Icons.alarm)),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: scaleWidth(context, 24)),
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Colors.grey.shade700,
+                    size: scaleWidth(context, 32),
+                  ),
+                ),
+              ),
+              Container(
+                width: scaleWidth(context, 80),
+                height: scaleWidth(context, 80),
+                margin: EdgeInsets.all(scaleWidth(context, 8)),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/pump.png'),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(scaleWidth(context, 8))),
+              ),
             ],
           ),
-        ),
-      ],
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: scaleWidth(context, 24)),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(child: _buildPumpDevice('History', Icons.history)),
+                  Expanded(child: _buildPumpDevice('Timer', Icons.timer)),
+                  Expanded(child: _buildPumpDevice('Alarm', Icons.alarm)),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   Widget _buildPumpDevice(String name, IconData icon) {
-    final size = MediaQuery.of(context).size;
     return Container(
       child: Column(
         children: <Widget>[
           Container(
-            width: size.width / 3,
-            height: size.width / 3,
-            padding: EdgeInsets.all(scaleWidth(context, 24)),
+            width: scaleWidth(context, 48),
+            height: scaleWidth(context, 48),
             child: OutlineButton(
               padding: EdgeInsets.zero,
               onPressed: () {},
@@ -85,10 +123,10 @@ class _DFamilyState extends State<DFamily> {
               shape: CircleBorder(),
               child: Container(
                   child: Icon(
-                    icon,
-                    color: Colors.black,
-                    size: scaleWidth(context, 50),
-                  )),
+                icon,
+                color: Colors.black,
+                size: scaleWidth(context, 30),
+              )),
             ),
           ),
           Container(
